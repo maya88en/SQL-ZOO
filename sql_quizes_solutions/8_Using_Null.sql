abea4bb5c68dd1f6b31662d8cea106abc248d70b
maya88en <maya88en@gmail.com>
2019-10-28 15:11:32 -0700

    using null quiz answers

--- sql_quizes_solutions/8_Using_Null.sql
+++ sql_quizes_solutions/8_Using_Null.sql
@@ -19,56 +19,37 @@ SELECT name,
 SELECT name FROM teacher
 WHERE dept IS NULL
 
---4. Note the INNER JOIN misses the teachers with no department and the departments with no teacher
+--4. Select the code which uses an outer join correctly.
 
-SELECT teacher.name, dept.name
-FROM teacher INNER JOIN dept
-ON (teacher.dept=dept.id)
+SELECT teacher.name, dept.name FROM teacher LEFT OUTER JOIN dept ON (teacher.dept = dept.id)
 
---5. Use a different JOIN so that all teachers are listed.
+--5. 
+ SELECT name, 
+      CASE 
+       WHEN dept 
+        IN (1) 
+        THEN 'Computing' 
+       ELSE 'Other' 
+      END 
+  FROM teacher
 
-SELECT teacher.name, dept.name
-FROM teacher LEFT JOIN dept
-ON (teacher.dept=dept.id);
+--Shrivell	Computing
+--Throd	Computing
+--Splint	Computing
+--Spiregrain	Other
+--Cutflower	Other
+--Deadyawn	Other
 
---6. Use a different JOIN so that all departments are listed.
+--6. Using SELECT name, dept, COALESCE(dept, 0) AS result FROM teacher on teacher table will:
 
-SELECT teacher.name, dept.name
-FROM teacher RIGHT JOIN dept
-ON (teacher.dept=dept.id);
+---- display 0 in result column for all teachers without department
 
---7. Use COALESCE to print the mobile number. Use the number '07986 444 2266' if there is no number given. Show teacher name and mobile number or '07986 444 2266'
+--7. Select the correct statement that shows the name of department which employs Cutflower -
 
-SELECT name, COALESCE (mobile, '07986 444 2266')
-FROM teacher;
+SELECT dept.name FROM teacher JOIN dept ON (dept.id = teacher.dept) WHERE teacher.name = 'Cutflower'
 
---8. Use the COALESCE function and a LEFT JOIN to print the teacher name and department name. Use the string 'None' where there is no department.
+--8. Select out of following the code which uses a JOIN to show a list of all the departments and number of employed teachers
 
-SELECT teacher.name,  COALESCE(dept.name, 'None')
-FROM teacher LEFT OUTER JOIN dept ON (teacher.dept=dept.id);
+SELECT dept.name, COUNT(teacher.name) FROM teacher RIGHT JOIN dept ON dept.id = teacher.dept GROUP BY dept.name
 
---9. Use COUNT to show the number of teachers and the number of mobile phones.
 
-SELECT COUNT(name), COUNT(mobile)
-FROM teacher;
-
---10. Use COUNT and GROUP BY dept.name to show each department and the number of staff. Use a RIGHT JOIN to ensure that the Engineering department is listed.
-
-SELECT dept.name, COUNT(teacher.id)
-FROM teacher
-RIGHT JOIN dept ON (teacher.dept = dept.id) 
-GROUP BY dept.name;
-
---11. Use CASE to show the name of each teacher followed by 'Sci' if the teacher is in dept 1 or 2 and 'Art' otherwise.
-
-SELECT name,
-CASE WHEN dept IN (1, 2) THEN 'Sci'ELSE 'Art' END
-FROM teacher;
-
---12. Use CASE to show the name of each teacher followed by 'Sci' if the teacher is in dept 1 or 2, show 'Art' if the teacher's dept is 3 and 'None' otherwise.
-
-SELECT name,
-CASE WHEN dept IN (1, 2) THEN 'Sci'
-WHEN dept IN (3) THEN 'Art'
-ELSE 'None'  END
-FROM teacher; 
